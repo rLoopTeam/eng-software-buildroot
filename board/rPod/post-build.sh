ce9a1d787315463a43be117335961f2124bbc575
Piense <piense@gmail.com>
2016-04-01 15:54:50 -0700

    Fix SD mount directories again
    Apparently git doesn't like empty directories so we need to create them
    manually during the build process.

--- board/rPod/post-build.sh
+++ board/rPod/post-build.sh
@@ -1,6 +1,10 @@
 TARGETDIR=$1
 BR_ROOT=$PWD
 
+mkdir $TARGETDIR/mnt/
+mkdir $TARGETDIR/mnt/boot
+mkdir $TARGETDIR/mnt/data
+
 install -T -m 0644 $BR_ROOT/system/skeleton/etc/fstab $TARGETDIR/etc/fstab
 echo '/dev/mmcblk0p1 /mnt/boot auto defaults 0 0' >> $TARGETDIR/etc/fstab
 echo '/dev/mmcblk0p2 /mnt/data auto defaults 0 0' >> $TARGETDIR/etc/fstab
