474851347181c8b1f75e531dc63f2a69b9fb48be
David <piense@gmail.com>
2016-06-18 22:07:47 -0700

    Add OpenVPN and GPIO IP.

---
+++ board/rPod/rootfs_overlay/bin/detectPiID.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.IN) #built in pullup
+GPIO.setup(3, GPIO.IN) #built in pullup
+GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(5, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(6, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(7, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+output = (1-GPIO.input(2))+2*(1-GPIO.input(3))+4*(1-GPIO.input(4))+8*(1-GPIO.input(5))+16*(1-GPIO.input(6))+32*(1-GPIO.input(7))
+print(output)
+
+GPIO.cleanup()
+
