62e26282af92a6da0c5e8089513fc5c3d96cd3e8
piense <piense@gmail.com>
2016-11-12 13:15:35 -0800

    Bring buildroot up to the uart code

--- package/rPod/bsc-slave-tools/bsc-slave-tools.mk
+++ package/rPod/bsc-slave-tools/bsc-slave-tools.mk
@@ -1,15 +1,14 @@
-BSC_SLAVE_TOOLS_VERSION = 99e763b74aeaf5b33947f6b2ff16b739ac83042a
+BSC_SLAVE_TOOLS_VERSION = cb843728fd84442040e93a66826fc9a8191d2f20
 BSC_SLAVE_TOOLS_SITE = $(call github,rLoopTeam,bsc-slave-tools,$(BSC_SLAVE_TOOLS_VERSION))
 BSC_SLAVE_TOOLS_INSTALL_TARGET = YES
-BSC_SLAVE_TOOLS_DEPENDENCIES = bsc-slave
 
 define BSC_SLAVE_TOOLS_BUILD_CMDS
 	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
 endef
 
 define BSC_SLAVE_TOOLS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/i2ccat $(TARGET_DIR)/bin
-	$(INSTALL) -D -m 0755 $(@D)/i2cSetParameter $(TARGET_DIR)/bin
+	$(INSTALL) -D -m 0755 $(@D)/uartSetParameter $(TARGET_DIR)/bin
+	$(INSTALL) -D -m 0755 $(@D)/uarttelemetry $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
--- package/rPod/bsc-slave/bsc-slave.mk
+++
@@ -1,8 +0,0 @@
-BSC_SLAVE_VERSION = 921d54ceefe2da6368381895e032fa08b139ee7c
-BSC_SLAVE_SITE = $(call github,rLoopTeam,bsc-slave,$(BSC_SLAVE_VERSION))
-BSC_SLAVE_INSTALL_TARGET = YES
-BSC_SLAVE_DEPENDENCIES = linux
-
-$(eval $(kernel-module))
-$(eval $(generic-package))
-
